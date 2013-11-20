c6283e88838451dccd318d71f35676259166138d
jsf62592 <jsf62592@comcast.net>
2013-11-20 11:29:38 -0500

    Added first few puzzles

---
+++ Assets/Standard Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	
+	public static int light = 0;
+	
+	// Use this for initialization
+	void Start () {
+		light = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void Light(){
+		light++;
+	}
+}
---
+++ Assets/Standard Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZone : MonoBehaviour {
+
+	public GUIText hint;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			if (Inventory.light == 1)
+			{
+				
+				Transform colPos = col.gameObject.transform;
+				colPos.position = new Vector3(transform.position.x, 55, transform.position.z);
+				colPos = col.gameObject.transform;
+				Destroy(GameObject.FindWithTag("LightSource"));
+			} else hint.SendMessage("ShowHint", "This looks like it can be jumped on to reach the " +
+				"next level, but it needs a light source to open up");
+		}
+	}
+}
