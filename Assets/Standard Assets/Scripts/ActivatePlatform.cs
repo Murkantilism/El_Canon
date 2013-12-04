42999c5efbb12ff2d79d48d784deb663056d71d3
jsf62592 <jsf62592@comcast.net>
2013-12-04 12:40:21 -0500

    Start of new puzzle

---
+++ Assets/Standard Assets/Scripts/ActivatePlatform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivatePlatform : MonoBehaviour {
+	
+	public GUIText switchHint;
+	public static bool shouldMove = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			switchHint.SendMessage("ShowHint", "Press Shift to activate the platform");
+		}
+	}
+}
---
+++ Assets/Standard Assets/Scripts/Platform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+	
+	public Transform origin;
+	public Transform destination;
+	bool back = false;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ActivatePlatform.shouldMove)
+		{
+			if(transform.position == destination.position)
+				back = true;
+			if(transform.position == origin.position)
+				back = false;
+			if(back)
+				transform.position = Vector3.MoveTowards(transform.position,
+					origin.position, speed);
+			else 
+				transform.position = Vector3.MoveTowards(transform.position,
+					destination.position, speed);
+		}
+	}
+}
